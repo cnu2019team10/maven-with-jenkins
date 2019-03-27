c523c5aafb4da9382b634870d18ae0c83c5eda4d
sglee487 <sglee487@gmail.com>
2019-03-27 19:04:38 +0900

    request

--- src/main/java/hello/SampleController.java
+++ src/main/java/hello/SampleController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import java.util.Scanner;
 
+// hello for request merge
+
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
